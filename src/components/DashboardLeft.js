f285d441d361edeceeb63e52dc69e6933d76fd22
Flatiron School <flatironschool@Flatirons-Air-2.home>
2017-09-21 23:18:35 -0400

    added links to left-nav

--- src/components/DashboardLeft.js
+++ src/components/DashboardLeft.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const DashboardLeft = (props) => {
 
   return (
-    <div className="dash-left">Left Nav</div>
+    <div className="dash-left">
+        <div className="logo">
+            INTRINSIC
+        </div>
+        <div className="avatar">
+            
+            <h3>Current User</h3>
+        </div>
+        <div className="left-nav">
+            <p>Navigate</p>
+            <Link to={`/dashboard/home`}>Dashboard</Link>
+            <Link to={`/dashboard/projects`}>My Projects</Link>
+            <Link to={`/dashboard/profile`}>My Profile</Link>
+            <Link to={`/dashboard/messages`}>Messages</Link>
+        </div>
+        <div className="left-nav">
+            <p>Navigate</p>
+            <Link to={`/dashboard/new`}>Create New</Link>
+            <Link to={`/dashboard/task`}>Add Task</Link>
+            <Link to={`/dashboard/profile`}>My Profile</Link>
+            <Link to={`/dashboard/messages`}>Messages</Link>
+        </div>
+    </div>
   )
 
 }
