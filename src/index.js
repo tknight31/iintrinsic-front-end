d79bef3c7d2e91ceac89195fe5ed0c40f9f66df3
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2017-10-03 19:30:12 -0400

    reverted messaging feature to seperate branch

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import projectsReducer from './reducers/projectsReducer'
 import usersReducer from './reducers/usersReducer'
 import skillsReducer from './reducers/skillsReducer'
-import chatReducer from './reducers/chatReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 
-const appReducer = combineReducers({users: usersReducer, projects: projectsReducer, skills: skillsReducer, chat: chatReducer})
+const appReducer = combineReducers({users: usersReducer, projects: projectsReducer, skills: skillsReducer})
 
 
 const rootReducer = (state, action) => {
